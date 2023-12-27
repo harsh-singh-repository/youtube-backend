eb6daac61540342e38de5b6e88b1c552157d0eca
Harsh <hk5425901@gmail.com>
2023-12-27 16:18:04 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import dotenv from "dotenv"; //this syntax is not good for code consistency.
 import mongoose from "mongoose";
 import { dbName } from "./constant.js";
 import connectDb from  "./db/index.js"
+import { app } from "./app.js";
 
 
 dotenv.config({
     path: "./env"
 })
 
-connectDb();
+connectDb()
+.then(()=>{
+     app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at ${process.env.PORT}`);
+     })
+}).catch((err)=>{
+  console.error("MONGODB connection Failed!",err);
+})
 
 
 
